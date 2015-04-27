f577326e4217e754d858a789970230b33064f91e
bryn85 <brynrich85@gmail.com>
2015-04-26 21:04:13 -0400

    Create pay_rate.py

---
+++ pay_rate.py
@@ -0,0 +1,8 @@
+#calculates emp pay as a function of hours and rate
+
+hrs = raw_input("Enter Hours:") 
+rt = raw_input ("Rate Per Hour")
+hours = float(hrs)
+rate = float(rt)
+pay = hours * rate 
+print pay
